794ee025b646b5d4164171404e1f4b8a499235e7
Sijuade Ajagunna <mahyor.sam@gmail.com>
2020-06-07 19:22:52 +0100

    add photo menu buttons

--- src/components/photo/photo.component.js
+++ src/components/photo/photo.component.js
@@ -12,9 +12,43 @@ function Photo({ count = 25 }) {
   const photoArr = [];
   for (let i = 1; i <= count; i++) {
     photoArr.push(
-      <figure className='img-brick' key={i}>
-        <img src={images[i % 5]} alt='placeholder img' />
-      </figure>
+      <div className='img' key={i}>
+        <figure className='img-brick'>
+          <img src={images[i % 5]} alt='placeholder img' />
+          <div className='img__btns'>
+            <div className='up'>
+              <div className='left'>
+                <a href='a' className='img__btns--btn'>
+                  <i className='fas fa-trash-alt'></i>
+                </a>
+              </div>
+              <div className='right'>
+                <a href='a' className='img__btns--btn'>
+                  <i className='fas fa-plus'></i>
+                </a>
+                <a href='a' className='img__btns--btn'>
+                  <i className='fas fa-arrow-down'></i>
+                </a>
+              </div>
+            </div>
+            <div className='down'>
+              <div className='left'>
+                <a href='a' className='img__btns--btn name'>
+                  <span className='title'>Killua Zoldyck</span>
+                </a>
+              </div>
+              <div className='right'>
+                <a href='a' className='img__btns--btn'>
+                  <i className='fas fa-heart'></i>
+                </a>
+                <a href='a' className='img__btns--btn'>
+                  <i className='far fa-comment'></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </figure>
+      </div>
     );
   }
   return <div className='masonry mx-auto'>{photoArr}</div>;
